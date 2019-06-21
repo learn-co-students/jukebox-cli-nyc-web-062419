0355810a656cad77328ba648baf789e6392776df
adriennem76 <adrienne.c.miranda@gmail.com>
2019-06-21 15:44:10 -0400

    jukebox

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -3,18 +3,22 @@
 #with the correct path to this directory on your computer
 
 # my_songs = {
-# "Go Go GO" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/01.mp3',
+# "Go Go GO" => '/Users/flatironschool/jukebox-cli-nyc-web-062419/audio/Emerald-Park/01.mp3',
 # "LiberTeens" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/02.mp3',
-# "Hamburg" =>  '< path to this directory >/jukebox-cli/audio/Emerald-Park/03.mp3',
-# "Guiding Light" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/04.mp3',
-# "Wolf" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/05.mp3',
-# "Blue" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/06.mp3',
-# "Graduation Failed" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/07.mp3'
+# "Hamburg" =>  '/Users/flatironschool/jukebox-cli-nyc-web-062419/audio/Emerald-Park/03.mp3',
+# "Guiding Light" => '/Users/flatironschool/jukebox-cli-nyc-web-062419/audio/Emerald-Park/04.mp3',
+# "Wolf" => '/Users/flatironschool/jukebox-cli-nyc-web-062419/audio/Emerald-Park/05.mp3',
+# "Blue" => '/Users/flatironschool/jukebox-cli-nyc-web-062419/audio/Emerald-Park/06.mp3',
+# "Graduation Failed" => '/Users/flatironschool/jukebox-cli-nyc-web-062419/audio/Emerald-Park/07.mp3'
 # }
 
 def help
   #this method should be the same as in jukebox.rb
-
+  puts "I accept the following commands:
+  - help : displays this help message
+  - list : displays a list of songs you can play
+  - play : lets you choose a song to play
+  - exit : exits this program"
 end
 
 
@@ -22,6 +26,9 @@ end
 def list(my_songs)
   #this method is different! Collect the keys of the my_songs hash and 
   #list the songs by name
+  my_songs.each do |song, file|
+    puts "#{song}"
+  end
 end
 
 
@@ -33,13 +40,36 @@ def play(my_songs)
   #if it isn't, tell them their choice is invalid
   #if it is, play the song using the system 'open <file path>' syntax
   #get the file path of the song by looking it up in the my_songs hash
-  
+  puts "Please enter a song name"
+  song_choice = gets.chomp
+  if my_songs.keys.include? (song_choice)
+    system "open #{my_songs[song_choice]}"
+    puts "Playing #{song_choice}"
+  else
+    puts "Invalid input, please try again."
+  end
 end
 
 def exit_jukebox
   #this method is the same as in jukebox.rb
+  puts "Goodbye"
 end
 
 def run(my_songs)
   #this method is the same as in jukebox.rb
+  help
+  loop do
+    puts "Please enter a command:"
+    command = gets.chomp
+    if command == "help"
+      help
+    elsif command == "list"
+      list(my_songs)
+    elsif command == "play"
+      play(my_songs)
+    elsif command == "exit"
+      exit
+      break
+    end
+  end
 end
--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 songs = [
   "Phoenix - 1901",
   "Tokyo Police Club - Wait Up",
@@ -10,3 +12,54 @@ songs = [
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
 
+def help
+  puts "I accept the following commands:
+  - help : displays this help message
+  - list : displays a list of songs you can play
+  - play : lets you choose a song to play
+  - exit : exits this program"
+end
+
+def list(songs)
+  songs.each_with_index do |song, index|
+    puts "#{index + 1}. #{song}"
+  end
+end
+
+def play(songs)
+  puts "Please enter a song name or number:"
+  song_choice = gets.chomp
+  if songs.include? (song_choice)
+      puts "Playing #{song_choice}"
+  elsif song_choice.to_i <= songs.length && song_choice.to_i > 0
+    songs.each_with_index do |song, index|
+      if song_choice.to_i == index + 1
+        puts "Playing #{song}"
+      end
+    end
+  else
+    puts "Invalid input, please try again."
+  end
+end
+
+def exit_jukebox
+  puts "Goodbye"
+end
+
+def run(songs)
+  help
+  loop do
+    puts "Please enter a command:"
+    command = gets.chomp
+    if command == "help"
+      help
+    elsif command == "list"
+      list(songs)
+    elsif command == "play"
+      play(songs)
+    elsif command == "exit"
+      exit
+      break
+    end
+  end
+end
